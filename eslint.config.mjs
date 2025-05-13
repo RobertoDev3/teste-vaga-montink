af885fa8b145e5da3e7311a8b77d8fbed5085596
RobertoDev3 <roberto.dev3@hotmail.com>
2025-05-13 18:49:41 -0300

    chore: update code structure and remove redundant sections

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,29 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      'react/prop-types': 'off',
+      'import/no-duplicates': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/camelcase': 'off',
+      'import/no-anonymous-default-export': [
+        'warn',
+        { allowObject: true, allowArrowFunction: true },
+      ],
+      semi: ['error'],
+      quotes: ['warn', 'single', { avoidEscape: true }],
+      'prefer-arrow-callback': ['error'],
+      'prefer-template': ['error'],
+    },
+  }),
 ];
 
 export default eslintConfig;
